f1bbed7314927de372208326a2c15c7b20ef8a34
superylcfly <350009335@qq.com>
2019-12-19 17:00:34 +0800

    tree

---
+++ src/main/java/org/ylc/structure/tree/BinaryTree.java
@@ -0,0 +1,18 @@
+package org.ylc.structure.tree;
+
+/**
+ * 代码千万行，注释第一行，
+ * 注释不规范，同事泪两行。
+ * <p>
+ * 二叉树
+ *
+ * @author YuLc
+ * @version 1.0.0
+ * @date 2019/12/18
+ */
+public class BinaryTree<E> {
+
+    public static void main(String[] args) {
+
+    }
+}
